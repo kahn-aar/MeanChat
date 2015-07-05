678de83b27a976a31fc8eb16fb3461b8bd4a187b
kahn-aar <nicolas.martinr@gmail.com>
2015-07-06 01:19:35 +0200

    Adding bootstrap

---
+++ public/application.js
@@ -0,0 +1,6 @@
+var mainApplicationModuleName = 'meanchat';
+var mainApplicationModule = angular.module(mainApplicationModuleName
+, []);
+angular.element(document).ready(function() {
+	angular.bootstrap(document, [mainApplicationModuleName]);
+});
